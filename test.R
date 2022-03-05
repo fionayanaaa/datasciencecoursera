c8506b9ce50486c22922177b93aa97b42ea11c7e
Fiona Yan <fionayan.yuetong@gmail.com>
2022-03-04 21:47:05 -0600

    quiz 1 data

--- test.R
+++ test.R
@@ -1,1 +1,23 @@
-print("test_repo")
+install.packages("plyr")
+library(plyr)
+setwd("/Users/fionayan/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Rstudio_Data")
+
+df <- read.csv("hw1_data.csv", header = TRUE, sep = ",")
+
+na_index <- is.na(df$Ozone)
+
+count(na_index)
+
+mean(df$Ozone[!na_index])
+
+idx <- (df$Ozone > 31) & (df$Temp > 90)
+df$Solar.R[idx]
+mean(df$Solar.R[idx], na.rm = TRUE)
+
+i <- df$Month == 6
+mean(df$Temp[i])
+
+max(df$Ozone[df$Month == 5], na.rm = TRUE)
+
+x <- list(2, "a", "b", TRUE)
+x[[1]]
